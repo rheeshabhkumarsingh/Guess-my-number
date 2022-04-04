58dc1962745725635cac63132c2a0b50e4e091ea
rheeshabh <rishabhs861@gmail.com>
2022-04-04 18:45:38 +0530

    finished game

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // let textContent = reference.textContent;
 // console.log(textContent);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let highscore_reference = document.querySelector('.highscore');
 let secretNumber_reference = document.querySelector('.number');
@@ -11,10 +11,12 @@ let message_reference = document.querySelector('.message');
 let input_reference = document.querySelector('.guess');
 let buttonCheck_reference = document.querySelector('.check');
 let score_reference = document.querySelector('.score');
+let again_reference = document.querySelector('.again');
+let body_reference = document.querySelector('body');
 
-let score = Number(score_reference.textContent);
+let score = 20;
+let highscore = 0;
 
-secretNumber_reference.textContent = secretNumber;
 buttonCheck_reference.addEventListener('click',() => {
     let guess = Number(input_reference.value);
     console.log(`guess: ${guess}`);
@@ -24,9 +26,15 @@ buttonCheck_reference.addEventListener('click',() => {
     else if( guess === secretNumber ) {
         message_reference.textContent = '🎉🥳 Correct Number!';
         highscore_reference.textContent = score;
+        if(highscore<score){
+            highscore = score;
+        }
+        body_reference.style.backgroundColor = '#60b';
+        secretNumber_reference.style.width = '30rem';
+        secretNumber_reference.textContent = secretNumber;
     }
     else if( guess < secretNumber) {
-        if(score>0){
+        if(score>=0){
             message_reference.textContent = '🔻 too less';
             score_reference.textContent = --score;
         }
@@ -35,7 +43,7 @@ buttonCheck_reference.addEventListener('click',() => {
         }
     }
     else if(guess > secretNumber){
-        if(score>0){
+        if(score>=0){
             message_reference.textContent = '🔼 too high';
             score_reference.textContent = --score;
         }
@@ -43,4 +51,15 @@ buttonCheck_reference.addEventListener('click',() => {
             message_reference.textContent = 'You lost the game!'
         }
     }
+});
+
+again_reference.addEventListener('click', () => {
+    secretNumber_reference.textContent = '?';
+    secretNumber_reference.style.width = '15rem';
+    message_reference.textContent = 'Start guessing...';
+    score_reference.textContent = '20';
+    score = 20;
+    body_reference.style.backgroundColor = '#222';
+    input_reference.value = '';
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
 })
