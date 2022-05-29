cd4f6c8fbc694f4f568ea0749d1edbfe3e0a6c02
rheeshabhkumarsingh@gmail.com <rheeshabhkumarsingh@gmail.com>
2022-05-29 16:13:04 +0530

    change secret number after play again clicked

--- script.js
+++ script.js
@@ -17,30 +17,30 @@ let body_reference = document.querySelector('body');
 let score = 20;
 let highscore = 0;
 
-buttonCheck_reference.addEventListener('click',() => {
+buttonCheck_reference.addEventListener('click', () => {
     let guess = Number(input_reference.value);
     console.log(`guess: ${guess}`);
-    if(!guess){
-        message_reference.innerHTML = '🔢 No number!';        
+    if (!guess) {
+        message_reference.innerHTML = '🔢 No number!';
     }
-    else if( guess === secretNumber ) {
+    else if (guess === secretNumber) {
         message_reference.textContent = '🎉🥳 Correct Number!';
         highscore_reference.textContent = score;
-        if(highscore<score){
+        if (highscore < score) {
             highscore = score;
         }
         body_reference.style.backgroundColor = '#60b';
         secretNumber_reference.style.width = '30rem';
         secretNumber_reference.textContent = secretNumber;
     }
-    else if(guess !== secretNumber){
-        if(score<=0){
+    else if (guess !== secretNumber) {
+        if (score <= 0) {
             message_reference.textContent = 'You lost the game!'
         }
-        else if(guess < secretNumber){
+        else if (guess < secretNumber) {
             message_reference.textContent = '🔻 too less';
         }
-        else if(guess > secretNumber){
+        else if (guess > secretNumber) {
             message_reference.textContent = '🔼 too high';
         }
         score_reference.textContent = --score;
@@ -63,6 +63,7 @@ buttonCheck_reference.addEventListener('click',() => {
 });
 
 again_reference.addEventListener('click', () => {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
     secretNumber_reference.textContent = '?';
     secretNumber_reference.style.width = '15rem';
     message_reference.textContent = 'Start guessing...';
